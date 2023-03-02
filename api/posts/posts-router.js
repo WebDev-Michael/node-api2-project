94e1a4a45f06f303b1c114ac979b6a6f197a446e
Krygore <webdev.krygore@gmail.com>
2023-03-01 20:51:27 -0800

    attempted all mvp

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -48,7 +48,31 @@ router.post('/', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-    
+    const { title, contents } = req.body
+    if (!title || !contents) {
+        res.status(400).json({message:"Please provide title and contents for the post"})
+    } else {
+        Post.findById(req.params.id)
+            .then(stuff => {
+                if (!stuff){
+                    res.status(404).json({message: "The post with the specified ID does not exist"})
+                } else{
+                    return Post.update(req.params.id, req.body)
+                }
+            })
+            .then(data => {
+                if(data) {
+                    return Post.findById(req.params.id)
+                }
+            })
+            .then(post =>{
+                res.json(post)
+            })
+            .catch (err => {
+                console.log(err)
+                res.status(500).json({message: 'The posts information could not be retrieved.'})
+                })
+    }
 })
 
 router.delete('/:id', async (req, res) => {
@@ -65,4 +89,17 @@ router.delete('/:id', async (req, res) => {
     }
 })
 
+router.get('/:id/messages', async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({message: 'The psot with the specified ID does not exist'})
+        } else {
+            const messages = await Post.findCommentById(req.params.id)
+            res.status(200).json(messages)
+        }
+    } catch (err) {
+        res.status(500).json({message:'The comments information could not be retrieved'})
+    }
+})
 module.exports = router
